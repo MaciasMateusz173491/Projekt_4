7b1f21dff43e5dce75f74c5e03757ea0ac68b478
MaciasMateusz173491 <173491@stud.prz.edu.pl>
2025-04-23 12:50:16 +0000

    4_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,35 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Podaj imię pierwszego studenta:");
+      String name1 = scanner.nextLine();
+      System.out.println("Podaj wiek pierwszego studenta:");
+      int age1 = scanner.nextInt();
+      scanner.nextLine();
+
+      System.out.println("Podaj imię drugiego studenta:");
+      String name2 = scanner.nextLine();
+      System.out.println("Podaj wiek drugiego studenta:");
+      int age2 = scanner.nextInt();
+
+      s.addStudent(new Student(name1, age1));
+      s.addStudent(new Student(name2, age2));
 
       var students = s.getStudents();
-      for(Student current : students) {
+      for (Student current : students) {
         System.out.println(current.ToString());
       }
-    } catch (IOException e) {
 
+    } catch (IOException e) {
+      System.out.println("Wystąpił błąd: " + e.getMessage());
     }
   }
 }
